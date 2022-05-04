aea7fa63204524cc824252cc5e78b22e3867381c
ezapretilina <e.zapretilina@sigma-it.ru>
2022-05-04 19:11:03 +0500

    hw02-generics

--- hw02-generics/src/main/java/homework/CustomerReverseOrder.java
+++ hw02-generics/src/main/java/homework/CustomerReverseOrder.java
@@ -1,17 +1,18 @@
 package homework;
 
+import java.util.Deque;
 import java.util.LinkedList;
 
-public class CustomerReverseOrder {
+public class CustomerReverseOrder <E>{
     //todo: 2. надо реализовать методы этого класса
-    LinkedList<Customer> lstCustomer = new LinkedList<>();
+    //LinkedList<Customer> lstCustomer = new LinkedList<>();
+    Deque<Customer> lstCustomer = new LinkedList<>();
 
     //надо подобрать подходящую структуру данных, тогда решение будет в "две строчки"
     public void add(Customer customer) {
         lstCustomer.push(customer);
-        }
-
+    }
     public Customer take() {
         return lstCustomer.pollFirst();
-        };
+    }
 }
