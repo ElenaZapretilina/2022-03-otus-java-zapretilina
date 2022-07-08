15569c81bd73421f54514888387ebcf7a1f9fcfc
ElenaZapretilina <e.zapretilina@sigma-it.ru>
2022-07-09 01:03:41 +0500

    hw10-byteCodes

--- hw10-byteCodes/src/main/java/Ioc.java
+++ hw10-byteCodes/src/main/java/Ioc.java
@@ -3,14 +3,15 @@ import annotations.Log;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.util.Arrays;
 
 class Ioc {
 
     private Ioc() {
     }
 
-    static TestLoggingInterface createMyClass() {
-        InvocationHandler handler = new DemoInvocationHandler(new TestLogging());
+    static TestLoggingInterface createMyClass(TestLoggingInterface testClass) {
+        InvocationHandler handler = new DemoInvocationHandler(testClass);
         return (TestLoggingInterface) Proxy.newProxyInstance(Ioc.class.getClassLoader(),
                 new Class<?>[]{TestLoggingInterface.class}, handler);
     }
@@ -28,7 +29,7 @@ class Ioc {
         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
             var hasAnnotation = myClass.getClass().getDeclaredMethod(method.getName(), method.getParameterTypes()).isAnnotationPresent(Log.class);
             if (hasAnnotation) {
-                System.out.println("invoking method:" + method);
+                System.out.println("invoking method:executed method: " + method.getName() + ", param:" + Arrays.toString(args));
             }
             return method.invoke(myClass, args);
         }
--- hw10-byteCodes/src/main/java/ProxyHW.java
+++ hw10-byteCodes/src/main/java/ProxyHW.java
@@ -2,7 +2,7 @@ import annotations.Log;
 
 public class ProxyHW {
     public static void main(String[] args) {
-        TestLoggingInterface myClassOne = Ioc.createMyClass();
+        TestLoggingInterface myClassOne = Ioc.createMyClass(new TestLogging());
         myClassOne.calculation(5);
         myClassOne.calculation(6, 7);
         myClassOne.calculation(6, 7, "end");
